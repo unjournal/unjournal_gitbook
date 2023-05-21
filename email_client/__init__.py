dff786595f81ab8208be8965acc7ad2150a6e392
Fernando Smith <66154723+fernandosmither@users.noreply.github.com>
2023-05-21 19:47:39 -0400

    wip(newsletter) add github actions trigger #PushNewsletter

--- email_client/__init__.py
+++ email_client/__init__.py
@@ -70,6 +70,7 @@ class EmailClient:
         response = self.__client.create_campaign(
             self.newsletter_type, self.newsletter_recipients, self.newsletter_settings  # type: ignore  # noqa: E501
         )
+        print(f"DEBUG: {response}")
         self.newsletter_campaign_id = response["id"]  # type: ignore
         return response
 
