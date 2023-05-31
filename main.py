5dc6def4aefabf5fbdc58c671d9eb79c18dc0dc4
Fernando Smith <66154723+fernandosmither@users.noreply.github.com>
2023-05-30 23:52:31 -0400

    newsletter: create newsletter specific code #PushNewsletter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from email_client import EmailClient
+from newsletter import get_email_contents
 
 
 email_client = EmailClient()
@@ -11,6 +12,8 @@ if __name__ == "__main__":
         title="Test email title",
         subject="Test email subject",
     )
+
     email_client.create_email()
-    email_client.set_newsletter_content("Test email plain text")
+    email_content = get_email_contents()
+    email_client.set_newsletter_content(email_content)
     email_client.send_email()
