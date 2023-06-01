bae8e906e4f2c7541ba9bbee13068c1d23dd91cb
Fernando Smith <66154723+fernandosmither@users.noreply.github.com>
2023-05-31 23:33:41 -0400

    newsletter: create markdown to html processor

--- newsletter/__init__.py
+++ newsletter/__init__.py
@@ -1,15 +1,35 @@
 """
 Newsletter specific functions
 """
+import markdown
+import re
+
+
+def pre_parse_md_from_gitbook(markdown: str) -> str:
+    # Parse {% embed url="" %} tags
+    for match in re.finditer(r"\{% embed url=[^%]*%\}", markdown):
+        embed_start, embed_end = match.span()
+        embed_tag = match.group()
+        embed_url = re.search(r"(?<=url=\").*?(?=\")", embed_tag).group()
+        markdown = markdown.replace(
+            embed_tag, f'<a href="{embed_url}">Read more</a>'
+        )
+    return markdown
 
 
 def get_email_contents() -> str:
-    email_head = ""
     email_body = ""
-    with open("HEAD.md", "r", encoding="utf-8") as file:
-        email_head = file.read()
-
     with open("UPDATES.md", "r", encoding="utf-8") as file:
-        email_body = file.read()
+        raw_body = file.read()
 
-    return email_head + email_body
+    try:
+        raw_body = pre_parse_md_from_gitbook(raw_body)
+    except Exception as error:
+        print("Could not pre-parse Gitbook markdown: ", error)
+
+    try:
+        email_body = markdown.markdown(raw_body)
+    except Exception as error:
+        print("Could not parse markdown into HTML: ", error)
+
+    return email_body
